43ecc3c7f413585e83885da6fe4ca332e5f1de2b
rajerison-jules <julesrajerison@gmail.com>
2020-11-06 00:59:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+AOS.init({
+    offset: 250,
+    duration: 1000
+});
+
+$(document).ready(function(){
+    $('#fullpage').fullpage({
+        autoScrolling:false,
+        scrollhorizontally: true,
+        navigation: true
+    });
+})
